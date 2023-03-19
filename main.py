0e216b8f17ab632dbac6b47e7d254e07b6b00b44
Pavel <PavelAB@bk.ru>
2023-03-19 20:25:04 +0300

    another zip demo

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def demo_zip():
     for v1, v2 in zip(values1, values2):
         print("v1 =", v1, ";  v2 =", v2, ";  sum =", v1+v2)
 
+    print("=" * 30)
+    values3 = list(range(20, -7, -5))
+    print(list(values3))
+    print(list(zip(values1, values2, values3)))
+
+    for v1, v2, v3 in zip(values1, values2, values3):
+        print("v1 =", v1, ";  v2 =", v2, ";  v3 =", v3, ";  sum =", v1 + v2 + v3)
+
 def main():
     # print("Hello main")
     demo_zip()
